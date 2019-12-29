2404420c9bbd89a9f820392b85f43d9f862b5f57
Christoph Kaiser <christoph.kaiser93@outlook.de>
2019-12-29 15:59:44 +0100

    CaretakerDao interface implemented

---
+++ app/src/main/java/de/htwg/mobilecomputing/caretakerapp/model/CaretakerDao.java
@@ -0,0 +1,27 @@
+package de.htwg.mobilecomputing.caretakerapp.model;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface CaretakerDao {
+
+    // Get all caretaker
+    @Query("SELECT * FROM caretaker")
+    List<Caretaker> getAll();
+
+    // Search for specific caretaker by mail
+    @Query("SELECT * FROM caretaker WHERE mEmail IN (:userIds)")
+    List<Caretaker> loadAllByIds(int[] userIds);
+
+    @Insert
+    void insertAll(Caretaker... users);
+
+    @Delete
+    void delete(Caretaker user);
+
+}
