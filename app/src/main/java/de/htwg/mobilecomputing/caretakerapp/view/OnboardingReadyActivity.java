42e8d9d8ba4f76483bff1e3614d4c2c57c887493
odsource <christoph.kaiser93@outlook.de>
2020-01-08 22:56:59 +0100

    Onboarding finished

--- app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingReadyActivity.java
+++ app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingReadyActivity.java
@@ -3,6 +3,7 @@ package de.htwg.mobilecomputing.caretakerapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,7 @@ import de.htwg.mobilecomputing.caretakerapp.R;
 import de.htwg.mobilecomputing.caretakerapp.databinding.ActivityOnboardingReadyBinding;
 
 public class OnboardingReadyActivity extends AppCompatActivity {
+    private String accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,15 @@ public class OnboardingReadyActivity extends AppCompatActivity {
         ActivityOnboardingReadyBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding_ready);
         binding.setLifecycleOwner(this);
 
+        Intent intent = getIntent();
+        accessToken = intent.getStringExtra(MainActivity.EXTRA_ACCESS_TOKEN);
+
         findViewById(R.id.onboarding_ready_cancel_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent startIntent = new Intent(OnboardingReadyActivity.this, DashboardActivity.class);
+                startIntent.putExtra(MainActivity.EXTRA_ACCESS_TOKEN, accessToken);
+                startActivity(startIntent);
             }
         });
     }
