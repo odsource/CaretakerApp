1631542a8b4d400436133e05f1c8f9fc9b3cd72c
odsource <christoph.kaiser93@outlook.de>
2020-01-08 19:31:57 +0100

    Goals view finished

--- app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingGoalsActivity.java
+++ app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingGoalsActivity.java
@@ -3,6 +3,7 @@ package de.htwg.mobilecomputing.caretakerapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,7 @@ import de.htwg.mobilecomputing.caretakerapp.R;
 import de.htwg.mobilecomputing.caretakerapp.databinding.ActivityOnboardingGoalsBinding;
 
 public class OnboardingGoalsActivity extends AppCompatActivity {
+    private String accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,15 @@ public class OnboardingGoalsActivity extends AppCompatActivity {
         ActivityOnboardingGoalsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding_goals);
         binding.setLifecycleOwner(this);
 
+        Intent intent = getIntent();
+        accessToken = intent.getStringExtra(MainActivity.EXTRA_ACCESS_TOKEN);
+
         findViewById(R.id.onboarding_goals_next_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent startIntent = new Intent(OnboardingGoalsActivity.this, OnboardingWorkDetailsActivity.class);
+                startIntent.putExtra(MainActivity.EXTRA_ACCESS_TOKEN, accessToken);
+                startActivity(startIntent);
             }
         });
     }
