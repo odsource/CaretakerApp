71058eba779176ce42c0f6413f5d15c62eeedfbd
odsource <christoph.kaiser93@outlook.de>
2020-01-08 22:47:00 +0100

    verification view finished

--- app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingVerificationActivity.java
+++ app/src/main/java/de/htwg/mobilecomputing/caretakerapp/view/OnboardingVerificationActivity.java
@@ -3,6 +3,7 @@ package de.htwg.mobilecomputing.caretakerapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,7 @@ import de.htwg.mobilecomputing.caretakerapp.R;
 import de.htwg.mobilecomputing.caretakerapp.databinding.ActivityOnboardingVerificationBinding;
 
 public class OnboardingVerificationActivity extends AppCompatActivity {
+    private String accesstoken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,15 @@ public class OnboardingVerificationActivity extends AppCompatActivity {
         ActivityOnboardingVerificationBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding_verification);
         binding.setLifecycleOwner(this);
 
+        Intent intent = getIntent();
+        accesstoken = intent.getStringExtra(MainActivity.EXTRA_ACCESS_TOKEN);
+
         findViewById(R.id.onboarding_verification_cancel_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent startIntent = new Intent(OnboardingVerificationActivity.this, OnboardingReadyActivity.class);
+                startIntent.putExtra(MainActivity.EXTRA_ACCESS_TOKEN, accesstoken);
+                startActivity(startIntent);
             }
         });
     }
